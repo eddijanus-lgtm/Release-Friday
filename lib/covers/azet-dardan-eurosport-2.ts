2ea478ea3d214e7642f93672b6abef764c11640b
eddijanus-lgtm <eddijanus@gmail.com>
2026-07-14 12:56:28 +0200

    Fix embedded Eurosport 2 cover assembly

--- lib/covers/azet-dardan-eurosport-2.ts
+++ lib/covers/azet-dardan-eurosport-2.ts
@@ -1,4 +1,7 @@
-import coverData from "./azet-dardan-eurosport-2/mobile-part-01";
+import part01 from "./azet-dardan-eurosport-2/mobile-part-01";
+import part02 from "./azet-dardan-eurosport-2/mobile-part-02";
+import part03 from "./azet-dardan-eurosport-2/mobile-part-03";
+import part04 from "./azet-dardan-eurosport-2/mobile-part-04";
 
 export const azetDardanEurosport2Cover =
-  `data:image/webp;base64,${coverData}`;
+  `data:image/webp;base64,${part01}${part02}${part03}${part04}`;
