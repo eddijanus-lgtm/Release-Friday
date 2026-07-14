555adc1213d9f37b0c47a33ba339447d45cdd2ae
eddijanus-lgtm <eddijanus@gmail.com>
2026-07-14 12:08:56 +0200

    Assemble embedded Eurosport 2 cover

---
+++ lib/covers/azet-dardan-eurosport-2.ts
@@ -0,0 +1,7 @@
+import part1 from "./azet-dardan-eurosport-2/part-1";
+import part2 from "./azet-dardan-eurosport-2/part-2";
+import part3 from "./azet-dardan-eurosport-2/part-3";
+import part4 from "./azet-dardan-eurosport-2/part-4";
+
+export const azetDardanEurosport2Cover =
+  `data:image/webp;base64,${part1}${part2}${part3}${part4}`;
